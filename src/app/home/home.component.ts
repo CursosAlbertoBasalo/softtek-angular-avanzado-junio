c23096bad2c0145981c63c3ae2aededd1c840614
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-21 19:00:16 +0200

    feat: container template context

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,5 @@
-import {
-  AfterViewInit,
-  ChangeDetectionStrategy,
-  Component,
-  OnChanges,
-  OnDestroy,
-  OnInit,
-  SimpleChanges,
-} from '@angular/core';
-import { Agency } from '@stk/models/agency.interface';
-import { Trip } from '@stk/models/trip.interface';
-import { Observable, Subscription } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable } from 'rxjs';
 import { HomeService } from './home.service';
 
 @Component({
@@ -18,36 +8,17 @@ import { HomeService } from './home.service';
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
-  public agencies: Agency[] = [];
-
-  public misDatos = [{ empresa: 'Softtek' }];
-
-  private subscriptions: Subscription[] = [];
-  // agencies$ = this.service.getAgencies$();
-  public trips$: Observable<Trip[]>; // = this.service.getTrips$();
+export class HomeComponent {
+  public agencies$: Observable<any>;
+  public trips$: Observable<any>;
 
   constructor(private service: HomeService) {
-    this.subscriptions.push(
-      this.service.getAgencies$().subscribe((agencies) => (this.agencies = agencies))
-    );
-
+    this.agencies$ = this.service.getAgencies$();
     this.trips$ = this.service.getTrips$();
-  }
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['entrada'].currentValue !== changes['entrada'].previousValue) {
-      console.log();
-    }
-  }
-  ngOnInit(): void {}
-  ngAfterViewInit(): void {}
-  ngOnDestroy(): void {
-    this.subscriptions.forEach((s) => s.unsubscribe());
   }
 
   loadAgencies() {
-    // this.agencies$ = this.service.getAgencies$();
-    this.service.getAgencies$().subscribe((agencies) => (this.agencies = agencies));
+    this.agencies$ = this.service.getAgencies$();
   }
   loadTrips() {
     this.trips$ = this.service.getTrips$();
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Agency } from '@stk/models/agency.interface';
 import { Trip } from '@stk/models/trip.interface';
 import { AgenciesService } from '@stk/services/agencies.service';
 import { TripsService } from '@stk/services/trips.service';
-import { delay, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of, pipe } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HomeService {
+  public errorPipe = pipe(
+    map((datos) => {
+      return { datos: datos, error: null };
+    }),
+    catchError((error) => {
+      return of({ datos: null, error: { dataName: 'algo', errorMessage: error.message } });
+    })
+  );
+
   constructor(private agencies: AgenciesService, private trips: TripsService) {}
-  getAgencies$(): Observable<Agency[]> {
-    return this.agencies.getAll$().pipe(delay(1500));
+  getAgencies$(): Observable<any> {
+    return this.agencies.getAll$().pipe(delay(1500)).pipe(this.errorPipe);
     // return this.getEmpty$();
     // return this.getError$();
   }
@@ -25,6 +33,6 @@ export class HomeService {
     return of([]).pipe(delay(1000));
   }
   private getError$(): Observable<any> {
-    return this.agencies.getError$();
+    return this.agencies.getError$().pipe(this.errorPipe);
   }
 }
