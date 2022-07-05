7296883138ad6f0ac5ed4347f805959445433cb0
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-05 14:46:33 +0200

    feat: cache

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -4,7 +4,7 @@ import { Response } from '@stk/models/response.interface';
 import { Trip } from '@stk/models/trip.interface';
 import { AgenciesService } from '@stk/services/agencies.service';
 import { TripsService } from '@stk/services/trips.service';
-import { delay, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,12 +13,11 @@ export class HomeService {
   constructor(private agencies: AgenciesService, private trips: TripsService) {}
 
   public getAgencies$(): Observable<Response<Agency[]>> {
-    // return this.agencies.getAll$().pipe(delay(1500));
-    return this.agencies.getEmpty$();
+    return this.agencies.getAll$().pipe();
     // return this.agencies.getError$();
   }
   public getTrips$(): Observable<Response<Trip[]>> {
-    return this.trips.getAll$().pipe(delay(500));
+    return this.trips.getAll$().pipe();
     // return this.trips.getEmpty$();
     // return this.trips.getError$();
   }
