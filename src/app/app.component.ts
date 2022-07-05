9d7c180740b63068cd64bf45a308a491371e8d06
Alberto Basalo <albertobasalo@hotmail.com>
2022-07-05 14:31:16 +0200

    feat: version control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate, VersionEvent } from '@angular/service-worker';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'stk-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   // title = 'softtek-angular-avanzado-abril';
+
+  public newVersion = '';
+
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.versionUpdates.subscribe((event: VersionEvent) => {
+      if (event.type === 'VERSION_READY') {
+        const version = event.latestVersion;
+        this.newVersion = version.appData ? JSON.stringify(version.appData) : version.hash;
+      }
+    });
+    interval(1000 * 60).subscribe(() => swUpdate.checkForUpdate());
+  }
+
+  public onReload() {
+    window.location.reload();
+  }
 }
